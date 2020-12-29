3df331a4dc565b925a4845a495534dd851f02ad1
tcorcor1 <tcorcor1@outlook.com>
2020-12-29 00:26:40 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Tooling.Connector;
+using Microsoft.Xrm.Sdk.Messages;
+
+namespace D365_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Setup
+            Console.WriteLine("Creating Service Client");
+            CrmServiceClient oMSCRMConn = new CrmServiceClient(ConfigurationManager.ConnectionStrings["tldr-profile1"].ConnectionString);
+            Console.WriteLine("Setting Up Org Service");
+            IOrganizationService service = (IOrganizationService)oMSCRMConn.OrganizationWebProxyClient != null ? (IOrganizationService)oMSCRMConn.OrganizationWebProxyClient : (IOrganizationService)oMSCRMConn.OrganizationServiceProxy;
+            Console.WriteLine(oMSCRMConn.IsReady ? "Connected" : "Failed to Connect");
+            Console.WriteLine($"Current User: {((WhoAmIResponse)service.Execute(new WhoAmIRequest())).UserId}");
+            #endregion
+
+            //Write
+            //Some
+            //Code
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Console App Sample for Dynamics 365
+
+## Documentation
+Please see the following for information on how to use this module:
+
+<https://tldr-dynamics.com/blog/console-app-sample>
